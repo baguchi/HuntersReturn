76106e2e0602cdd8313c6344afb9d995f1045852
baguchan <shurf012@gmail.com>
2021-06-21 19:18:27 +0900

    misc change

--- src/main/java/baguchan/hunterillager/init/HunterEntityRegistry.java
+++ src/main/java/baguchan/hunterillager/init/HunterEntityRegistry.java
@@ -12,9 +12,9 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = "hunterillager", bus = EventBusSubscriber.Bus.MOD)
 public class HunterEntityRegistry {
-	public static final EntityType<HunterIllagerEntity> HUNTERILLAGER = EntityType.Builder.of(HunterIllagerEntity::new, EntityClassification.CREATURE).setTrackingRange(80).setUpdateInterval(3).setShouldReceiveVelocityUpdates(true).sized(0.6F, 1.95F).build(prefix("hunterillager"));
+	public static final EntityType<HunterIllagerEntity> HUNTERILLAGER = EntityType.Builder.of(HunterIllagerEntity::new, EntityClassification.CREATURE).sized(0.6F, 1.95F).build(prefix("hunterillager"));
 
-	public static final EntityType<BoomerangEntity> BOOMERANG = EntityType.Builder.<BoomerangEntity>of(BoomerangEntity::new, EntityClassification.MISC).setTrackingRange(60).setUpdateInterval(1).setShouldReceiveVelocityUpdates(true).setCustomClientFactory(BoomerangEntity::new).sized(0.4F, 0.2F).build(prefix("boomerang"));
+	public static final EntityType<BoomerangEntity> BOOMERANG = EntityType.Builder.<BoomerangEntity>of(BoomerangEntity::new, EntityClassification.MISC).setCustomClientFactory(BoomerangEntity::new).sized(0.4F, 0.2F).build(prefix("boomerang"));
 
 	@SubscribeEvent
 	public static void registerEntity(RegistryEvent.Register<EntityType<?>> event) {
