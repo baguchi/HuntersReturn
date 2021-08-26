4c6f61985d59d31f984548418bf484551b4ddc5a
baguchan <shurf012@gmail.com>
2021-08-27 04:41:02 +0900

    fixed hunter model

--- src/main/java/baguchan/hunterillager/client/model/HunterIllagerModel.java
+++ src/main/java/baguchan/hunterillager/client/model/HunterIllagerModel.java
@@ -20,7 +20,7 @@ public class HunterIllagerModel<T extends HunterIllagerEntity> extends IllagerMo
 		super(p_170688_);
 		this.body = p_170688_.getChild("body");
 		this.cape = this.body.getChild("cape");
-		this.quiver = this.body.getChild("quiver");
+		this.quiver = this.cape.getChild("quiver");
 		this.capeLower = this.cape.getChild("cape_lower");
 		this.getHead().visible = true;
 		/*getHat().addBox("hood_fixed", -4.0F, -10.0F, -4.0F, 8, 10, 8, scaleFactor + 0.45F, 32, 0);
@@ -57,8 +57,8 @@ public class HunterIllagerModel<T extends HunterIllagerEntity> extends IllagerMo
 		partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(40, 46).mirror().addBox(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F), PartPose.offset(5.0F, 2.0F, 0.0F));
 
 		PartDefinition partdefinition5 = partdefinition4.addOrReplaceChild("cape", CubeListBuilder.create().texOffs(0, 64).addBox(-4.5F, 0.0F, -0.5F, 9.0F, 11.0F, 1.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 0.5F, 3.0F));
-		partdefinition4.addOrReplaceChild("quiver", CubeListBuilder.create().texOffs(20, 64).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 13.0F, 3.0F, new CubeDeformation(-0.5F)).texOffs(36, 64).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 13.0F, 3.0F), PartPose.offset(3.0F, 0.0F, 2.5F));
-		partdefinition5.addOrReplaceChild("cape_lower", CubeListBuilder.create().texOffs(0, 76).addBox(-4.5F, 0.0F, -0.5F, 9.0F, 8.0F, 1.0F).texOffs(0, 38).addBox(-4.0F, 0.0F, -3.0F, 8.0F, 18.0F, 6.0F, new CubeDeformation(0.1F)), PartPose.offset(0.0F, 11.0F, 0.0F));
+		partdefinition5.addOrReplaceChild("quiver", CubeListBuilder.create().texOffs(20, 64).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 13.0F, 3.0F, new CubeDeformation(-0.5F)).texOffs(36, 64).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 13.0F, 3.0F), PartPose.offset(3.0F, 0.0F, 2.5F));
+		partdefinition5.addOrReplaceChild("cape_lower", CubeListBuilder.create().texOffs(0, 76).addBox(-4.5F, 0.0F, -0.5F, 9.0F, 8.0F, 1.0F), PartPose.offset(0.0F, 11.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 128);
 	}
