9deb77c5f1b60209cf5a7440d3a28ed59a833fdd
baguchan <shutux500@gmail.com>
2024-03-26 16:21:22 +0900

    fixed hunter model layer location(should be can do the compat)

--- src/main/java/baguchan/hunters_return/client/ModModelLayers.java
+++ src/main/java/baguchan/hunters_return/client/ModModelLayers.java
@@ -5,7 +5,5 @@ import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModModelLayers {
-    public static ModelLayerLocation HUNTER = new ModelLayerLocation(new ResourceLocation(HuntersReturn.MODID, "hunter"), "hunter");
-    public static ModelLayerLocation RUDEHOG = new ModelLayerLocation(new ResourceLocation(HuntersReturn.MODID, "rudehog"), "rudehog");
-    public static ModelLayerLocation SPIN_BLADE = new ModelLayerLocation(new ResourceLocation(HuntersReturn.MODID, "spin_blade"), "spin_blade");
+    public static ModelLayerLocation HUNTER = new ModelLayerLocation(new ResourceLocation(HuntersReturn.MODID, "hunter"), "main");
 }
