68198c4c64e0619b71551fb4c5711ac37f11c4ea
baguchan <shutux500@gmail.com>
2024-06-18 21:54:55 +0900

    add raid

---
+++ src/main/java/baguchan/hunters_return/init/RaiderEnumParams.java
@@ -0,0 +1,11 @@
+package baguchan.hunters_return.init;
+
+import net.minecraft.world.entity.raid.Raid;
+import net.neoforged.fml.common.asm.enumextension.EnumProxy;
+
+public class RaiderEnumParams {
+    @SuppressWarnings("unused")
+    public static final EnumProxy<Raid.RaiderType> HUNTER = new EnumProxy<>(
+            Raid.RaiderType.class, HunterEntityRegistry.HUNTERILLAGER, new int[]{0, 1, 2, 2, 2, 2, 3, 3}
+    );
+}
