ccefb08222744dd664437c16acdff0a28570d999
baguchan <shutux500@gmail.com>
2025-08-29 22:43:07 +0900

    1.21.8-26.0.1 fix weird strafe

--- src/main/java/baguchi/hunters_return/entity/ai/DodgeGoal.java
+++ src/main/java/baguchi/hunters_return/entity/ai/DodgeGoal.java
@@ -36,7 +36,7 @@ public class DodgeGoal extends Goal {
 					return (p_148124_.getOwner() == this.hunter.getTarget() && (p_148124_.getOwner() == null || !this.hunter.isAlliedTo(p_148124_.getOwner())));
 				});
 				if (!toAvoid.isEmpty()) {
-                    this.cooldownTime = 40 + this.hunter.getRandom().nextInt(2) * 20;
+                    this.cooldownTime = 20 + this.hunter.getRandom().nextInt(2) * 10;
 					return true;
 				}
 			}
