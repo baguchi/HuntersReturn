7ee24f321699eff9ede4d12a528f921627622b27
baguchan <shutux500@gmail.com>
2025-12-25 08:04:31 +0900

    1.21.11-28.2.0 support another mob charge duration small when using mini crossbow

---
+++ src/main/java/baguchi/hunters_return/mixin/CrossbowItemMixin.java
@@ -0,0 +1,28 @@
+package baguchi.hunters_return.mixin;
+
+import baguchi.hunters_return.item.MiniCrossbowItem;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ProjectileWeaponItem;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CrossbowItem.class)
+public abstract class CrossbowItemMixin extends ProjectileWeaponItem {
+    public CrossbowItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "getChargeDuration", at = @At("HEAD"), cancellable = true)
+    private static void getChargeDuration(ItemStack stack, LivingEntity shooter, CallbackInfoReturnable<Integer> cir) {
+        if (stack.getItem() instanceof MiniCrossbowItem) {
+            float f = EnchantmentHelper.modifyCrossbowChargingTime(stack, shooter, 0.65F);
+            cir.setReturnValue(Mth.floor(f * 20.0F));
+        }
+    }
+}
