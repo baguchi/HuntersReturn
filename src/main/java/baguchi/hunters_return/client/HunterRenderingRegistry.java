0611cac6272010f3af23ad8d6bcbdeda61321dbf
baguchan <shutux500@gmail.com>
2026-02-08 13:31:34 +0900

    1.21.11-28.7.0

--- src/main/java/baguchi/hunters_return/client/HunterRenderingRegistry.java
+++ src/main/java/baguchi/hunters_return/client/HunterRenderingRegistry.java
@@ -1,18 +1,14 @@
 package baguchi.hunters_return.client;
 
-import baguchi.hunters_return.HuntersReturn;
 import baguchi.hunters_return.client.model.HunterModel;
 import baguchi.hunters_return.client.model.OldHunterModel;
 import baguchi.hunters_return.client.render.BoomerangRender;
 import baguchi.hunters_return.client.render.HunterRender;
-import baguchi.hunters_return.client.render.item.properties.MiniCrossbowPull;
 import baguchi.hunters_return.init.HunterEntityRegistry;
-import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import net.neoforged.neoforge.client.event.RegisterRangeSelectItemModelPropertyEvent;
 
 
 @EventBusSubscriber(modid = baguchi.hunters_return.HuntersReturn.MODID, value = Dist.CLIENT)
@@ -27,10 +23,5 @@ public class HunterRenderingRegistry {
     public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(ModModelLayers.HUNTER, HunterModel::createBodyLayer);
         event.registerLayerDefinition(ModModelLayers.HUNTER_OLD, OldHunterModel::createBodyLayer);
-    }
-
-    @SubscribeEvent
-    public static void registerRangeModelProperty(RegisterRangeSelectItemModelPropertyEvent event) {
-        event.register(Identifier.fromNamespaceAndPath(HuntersReturn.MODID, "mini_crossbow/pull"), MiniCrossbowPull.MAP_CODEC);
     }
 }
