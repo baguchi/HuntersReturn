03a976f246f83eef5ecc89f0c72e91e307c0c324
baguchan <shutux500@gmail.com>
2026-02-14 18:11:26 +0900

    1.21.1-20.6.0 fil_ph.json lang update(Thanks Francis97979!) and hunter spawn egg id changed

--- src/main/java/baguchan/hunters_return/init/HunterItems.java
+++ src/main/java/baguchan/hunters_return/init/HunterItems.java
@@ -12,7 +12,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class HunterItems {
 	public static final DeferredRegister.Items ITEM_REGISTRY = DeferredRegister.createItems(baguchan.hunters_return.HuntersReturn.MODID);
 
-	public static final DeferredItem<DeferredSpawnEggItem> SPAWNEGG_HUNTER = ITEM_REGISTRY.register("spawnegg_hunter", () -> new DeferredSpawnEggItem(HunterEntityRegistry.HUNTERILLAGER, 9804699, 5777447, (new Item.Properties())));
+	public static final DeferredItem<DeferredSpawnEggItem> SPAWNEGG_HUNTER = ITEM_REGISTRY.register("hunter_spawn_egg", () -> new DeferredSpawnEggItem(HunterEntityRegistry.HUNTERILLAGER, 9804699, 5777447, (new Item.Properties())));
 	public static final DeferredItem<Item> BOOMERANG = ITEM_REGISTRY.register("boomerang", () -> new BoomerangItem((new Item.Properties()).durability(384)));
 	public static final DeferredItem<Item> MINI_CROSSBOW = ITEM_REGISTRY.register("mini_crossbow", () -> new MiniCrossBowItem((new Item.Properties().durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
 
