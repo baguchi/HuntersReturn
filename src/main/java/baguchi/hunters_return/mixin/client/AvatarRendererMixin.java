d3dbf1234939abf92cf046d2ccef7fae4dcba6c5
baguchi <shutux500@gmail.com>
2026-08-12 16:56:34 +0900

    fixed arm pose after charge

---
+++ src/main/java/baguchi/hunters_return/mixin/client/AvatarRendererMixin.java
@@ -0,0 +1,24 @@
+package baguchi.hunters_return.mixin.client;
+
+import baguchi.hunters_return.init.HunterItems;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Avatar;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AvatarRenderer.class)
+public class AvatarRendererMixin {
+
+    @Inject(method = "getArmPose(Lnet/minecraft/world/entity/Avatar;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/client/model/HumanoidModel$ArmPose;", at = @At("HEAD"), cancellable = true)
+    private static void getArmPose(Avatar avatar, ItemStack itemInHand, InteractionHand hand, CallbackInfoReturnable<HumanoidModel.ArmPose> cir) {
+        if (!avatar.swinging && itemInHand.is(HunterItems.MINI_CROSSBOW) && CrossbowItem.isCharged(itemInHand)) {
+            cir.setReturnValue(HumanoidModel.ArmPose.CROSSBOW_HOLD);
+        }
+    }
+}
